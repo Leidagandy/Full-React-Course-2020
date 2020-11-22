f1066231283ce2e9d51edd45222cc2a562f54632
Leida_Gandy <leida.gandy@gmail.com>
2020-11-22 11:19:48 +0000

    first commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,58 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDom from "react-dom";
+import "./index.css";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const firstBook = {
+  img:
+    "https://images-na.ssl-images-amazon.com/images/I/81eB%2B7%2BCkUL._AC_UL200_SR200,200_.jpg",
+  title: "Amelia Hepworth",
+  author: "I Love You to the Moon and Back",
+};
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const secondBook = {
+  img:
+    "https://m.media-amazon.com/images/I/71aLultW5EL._AC_UY327_FMwebp_QL65_.jpg",
+  title: "God Gave Us You",
+  author: "Lisa Tawn Bergren",
+};
+const thirdBook = {
+  img:
+    "https://images-na.ssl-images-amazon.com/images/I/61oHAXZX7ML._SX382_BO1,204,203,200_.jpg",
+  title: "How to Draw 101 Animals",
+  author: "Nat Lambert",
+};
+
+function BookList() {
+  return (
+    <section className="booklist">
+      <Book
+        img={firstBook.img}
+        title={firstBook.title}
+        author={firstBook.author}
+      />
+      <Book
+        img={secondBook.img}
+        title={secondBook.title}
+        author={secondBook.author}
+      />
+      <Book
+        img={thirdBook.img}
+        title={thirdBook.title}
+        author={thirdBook.author}
+      />
+    </section>
+  );
+}
+
+const Book = (props) => {
+  // console.log(props);
+  return (
+    <article className="book">
+      <img src={props.img} alt="I love you to the moon and back book" />
+      <h1>{props.title}</h1>
+      <h4>{props.author}</h4>
+    </article>
+  );
+};
+
+ReactDom.render(<BookList />, document.getElementById("root"));
