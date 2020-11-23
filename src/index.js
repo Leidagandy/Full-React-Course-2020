e1183dd56f03a713f4cbe6ae9e9af2cfd25ba05f
Leida_Gandy <leida.gandy@gmail.com>
2020-11-23 09:39:19 +0000

    Add event handlers

--- src/index.js
+++ src/index.js
@@ -2,55 +2,78 @@ import React from "react";
 import ReactDom from "react-dom";
 import "./index.css";
 
-const firstBook = {
-  img:
-    "https://images-na.ssl-images-amazon.com/images/I/81eB%2B7%2BCkUL._AC_UL200_SR200,200_.jpg",
-  title: "Amelia Hepworth",
-  author: "I Love You to the Moon and Back",
-};
+const books = [
+  {
+    id: 1,
+    img:
+      "https://images-na.ssl-images-amazon.com/images/I/81eB%2B7%2BCkUL._AC_UL200_SR200,200_.jpg",
+    title: "I Love You to the Moon and Back",
+    author: "Amelia Hepworth",
+  },
 
-const secondBook = {
-  img:
-    "https://m.media-amazon.com/images/I/71aLultW5EL._AC_UY327_FMwebp_QL65_.jpg",
-  title: "God Gave Us You",
-  author: "Lisa Tawn Bergren",
-};
-const thirdBook = {
-  img:
-    "https://images-na.ssl-images-amazon.com/images/I/61oHAXZX7ML._SX382_BO1,204,203,200_.jpg",
-  title: "How to Draw 101 Animals",
-  author: "Nat Lambert",
-};
+  {
+    id: 2,
+    img:
+      "https://m.media-amazon.com/images/I/71aLultW5EL._AC_UY327_FMwebp_QL65_.jpg",
+    title: "God Gave Us You",
+    author: "Lisa Tawn Bergren",
+  },
+
+  {
+    id: 3,
+    img:
+      "https://images-na.ssl-images-amazon.com/images/I/61oHAXZX7ML._SX382_BO1,204,203,200_.jpg",
+    title: "How to Draw 101 Animals",
+    author: "Nat Lambert",
+  },
+  {
+    id: 4,
+    img:
+      "https://images-na.ssl-images-amazon.com/images/I/61xTjuVrwsL._SX375_BO1,204,203,200_.jpg",
+    title: "Big Preschool Workbook",
+    author: "Joan Hoffman ",
+  },
+];
 
 function BookList() {
   return (
     <section className="booklist">
-      <Book
-        img={firstBook.img}
-        title={firstBook.title}
-        author={firstBook.author}
-      />
-      <Book
-        img={secondBook.img}
-        title={secondBook.title}
-        author={secondBook.author}
-      />
-      <Book
-        img={thirdBook.img}
-        title={thirdBook.title}
-        author={thirdBook.author}
-      />
+      {books.map((book) => {
+        return <Book key={book.id} {...book} />;
+      })}
     </section>
   );
 }
-
 const Book = (props) => {
-  // console.log(props);
+  //attribute , eventHandler
+  //onClick, onMouseOver
+  const clickHandler = (e) => {
+    console.log(e);
+    console.log(e.target);
+    alert("Heloo");
+  };
+  const complexExample = (author) => {
+    console.log(author);
+  };
+
+  const { img, title, author } = props;
+  console.log(props);
   return (
-    <article className="book">
-      <img src={props.img} alt="I love you to the moon and back book" />
-      <h1>{props.title}</h1>
-      <h4>{props.author}</h4>
+    <article
+      className="book"
+      onMouseOver={() => {
+        console.log(title);
+      }}
+    >
+      <img src={img} alt="" />
+      <h1 onClick={() => console.log(title)}>{title}</h1>
+      <h4>{author}</h4>
+      <button type="button" onClick={clickHandler}>
+        Reference Example
+      </button>
+      <button type="button" onClick={() => complexExample(author)}>
+        Complex Example
+      </button>
     </article>
   );
 };
